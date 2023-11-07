f3023c07e0e49949d3b0fc44ec665f07fa3d9064
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-07 14:55:51 -0300

    install cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const knex = require("knex")
 
+const cors = require("cors")
 const app = express()
 const routes = require("./routes")
 
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
