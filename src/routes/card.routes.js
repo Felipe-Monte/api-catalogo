68bab168d501ef4a5fe44bb949a2d945db124038
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-10 14:19:24 -0300

    creating image route

--- src/routes/card.routes.js
+++ src/routes/card.routes.js
@@ -1,12 +1,20 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const CardsController = require("../controller/CardsController")
 const cardsController = new CardsController()
 
 const cardsRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 cardsRoutes.post("/", cardsController.create)
 cardsRoutes.get("/", cardsController.index)
+
+cardsRoutes.post("/image", upload.single("image"), (request, response) => {
+  console.log(request.file.filename)
+  response.json()
+})
 
 
 module.exports = cardsRoutes
