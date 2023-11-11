65be5474af7cd811dcabea59ef6faa0262b49e96
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-11 10:59:40 -0300

    testing send image

--- src/controller/CardsImageController.js
+++ src/controller/CardsImageController.js
@@ -1,7 +1,45 @@
-
+const knex = require("knex");
+const knexfile = require("../../knexfile");
+const DiskStorage = require("../providers/DiskStorage");
+const db = knex(knexfile);
 
 class CardsImageController {
-      
-} 
+  async create(request, response) {
+    const cards = await db('cards').where("id",2).select("*")
+    console.log(cards)
+    // try {
+    //   const cardId = request.body.cardId;
+    
+    //   const imageFileName = request.file.filename;
 
-module.exports = CardsImageController
+    //   const diskStorage = new DiskStorage();
+
+    //   // Consulta o card no banco de dados
+    //   const card = await db("cards").where( "id", cardId ).first();
+
+    //   // Verifica se o card existe
+    //   if (!card) {
+    //     return response.status(404).json({ error: 'Card não encontrado.' });
+    //   }
+
+    //   // Deleta a imagem antiga, se existir
+    //   if (card.imgUrl) {
+    //     await diskStorage.deleteFile(card.imgUrl);
+    //   }
+
+    //   // Salva a nova imagem e atualiza o campo imgUrl no card
+    //   const newImageUrl = await diskStorage.saveFile(imageFileName);
+    //   card.imgUrl = newImageUrl;
+
+    //   // Atualiza o card no banco de dados
+    //   await db("cards").where({ id: cardId }).update({ imgUrl: newImageUrl });
+
+    //   return response.json(card);
+    // } catch (error) {
+    //   console.error(error);
+    //   return response.status(500).json({ error: 'Erro interno no servidor.' });
+    // }
+  }
+}
+
+module.exports = CardsImageController;
--- src/routes/card.routes.js
+++ src/routes/card.routes.js
@@ -3,7 +3,10 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const CardsController = require("../controller/CardsController")
+const CardsImageController  = require("../controller/CardsImageController")
+
 const cardsController = new CardsController()
+const cardsImageController = new CardsImageController()
 
 const cardsRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
@@ -11,10 +14,7 @@ const upload = multer(uploadConfig.MULTER)
 cardsRoutes.post("/", cardsController.create)
 cardsRoutes.get("/", cardsController.index)
 
-cardsRoutes.post("/image", upload.single("image"), (request, response) => {
-  console.log(request.file.filename)
-  response.json()
-})
+cardsRoutes.post("/image", upload.single("image"), cardsImageController.create)
 
 
 module.exports = cardsRoutes
